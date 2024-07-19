ed1ee6cd1ea904c49fe82f13e47d341a40ed8556
Hamjad Idris <dajmahsirdi@gmail.com>
2024-07-19 23:37:27 +0100

    initial commit

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ var Engine = Matter.Engine,
   Runner = Matter.Runner,
   Bodies = Matter.Bodies,
   Composite = Matter.Composite;
+Constraint = Matter.Constraint;
+MouseConstraint = Matter.MouseConstraint;
+Mouse = Matter.Mouse;
 
 // create an engine
-var engine = Engine.create();
+var engine = Engine.create({
+  gravity: {
+    x: 0,
+    y: 0,
+  },
+});
 
 let screenWidth = window.innerWidth;
 let screenHeight = window.innerHeight;
@@ -36,7 +44,7 @@ var ground = Bodies.rectangle(
 // add all of the bodies to the world
 Composite.add(engine.world, ground);
 
-const box = (e) =>
+const box = () =>
   Bodies.rectangle(
     e.clientX,
     e.clientY,
@@ -44,13 +52,67 @@ const box = (e) =>
     Math.random() * 50
   );
 
-window.addEventListener("mousedown", (e) => {
-  Composite.add(engine.world, box(e));
+let cicleOptions = {
+  collisionFilter: {
+    group: -1,
+  },
+  restitution: 0.5,
+};
+
+const circle1 = Bodies.circle(
+  screenWidth / 2,
+  screenHeight / 2,
+  50,
+  cicleOptions
+);
+
+const circle2 = Bodies.circle(
+  screenWidth / 2,
+  screenHeight / 2,
+  10,
+  cicleOptions
+);
+
+const testConstraint = Constraint.create({
+  bodyA: circle1,
+  bodyB: circle2,
+  length: 50,
+  stiffness: 1,
 });
 
-window.addEventListener("touchstart", (e) => {
-  Composite.add(engine.world, box(e));
-});
+Composite.add(engine.world, circle1);
+Composite.add(engine.world, circle2);
+Composite.add(engine.world, testConstraint);
+
+// window.addEventListener("mousedown", (e) => {
+//   Composite.add(engine.world, box(e));
+// });
+
+// window.addEventListener("touchstart", (e) => {
+//   Composite.add(engine.world, box(e));
+// });
+
+var mouse = Mouse.create(render.canvas),
+  mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      stiffness: 0.2,
+      render: {
+        visible: false,
+      },
+    },
+  });
+
+Composite.add(engine.world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+// fit the render viewport to the scene
+// Render.lookAt(render, {
+//   min: { x: 0, y: 0 },
+//   max: { x: 800, y: 600 },
+// });
 
 // run the renderer
 Render.run(render);
@@ -60,3 +122,18 @@ var runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
+
+// var gyroPresent = false;
+// window.addEventListener("devicemotion", function(event){
+//     if(event.rotationRate.alpha || event.rotationRate.beta || event.rotationRate.gamma)
+//         gyroPresent = true;
+// });
+
+// let gyroscope = new Gyroscope({ frequency: 60 });
+
+// gyroscope.addEventListener("reading", (e) => {
+//   console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
+//   console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
+//   console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
+// });
+// gyroscope.start();
