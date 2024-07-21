042fdb729a1c3ab4ab95f516b517a7e4a663d462
Hamjad Idris <dajmahsirdi@gmail.com>
2024-07-21 01:48:19 +0100

    initial commit

--- script.js
+++ script.js
@@ -16,8 +16,10 @@ var engine = Engine.create({
   },
 });
 
-let screenWidth = window.innerWidth;
-let screenHeight = window.innerHeight;
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+const halfScreenWidth = window.innerWidth / 2;
+const halfScreenHeight = window.innerHeight / 2;
 
 let canvasOptions = {
   height: screenHeight,
@@ -32,69 +34,89 @@ var render = Render.create({
   options: canvasOptions,
 });
 
-var ground = Bodies.rectangle(
-  0 + screenWidth / 2,
-  screenHeight - 30,
-  screenWidth,
-  60,
-  {
-    isStatic: true,
-  }
-);
+let headCategory = 0x0001;
 
-// add all of the bodies to the world
-Composite.add(engine.world, ground);
-
-const box = () =>
-  Bodies.rectangle(
-    e.clientX,
-    e.clientY,
-    Math.random() * 50,
-    Math.random() * 50
-  );
-
-let cicleOptions = {
-  collisionFilter: {
-    group: -1,
+const cicleOptions = {
+  frictionAir: 0.5,
+  render: {
+    lineWidth: 2,
+    fillStyle: "transparent",
   },
   restitution: 0.5,
 };
 
-const circle1 = Bodies.circle(
-  screenWidth / 2,
-  screenHeight / 2,
-  50,
-  cicleOptions
+// Make an array of circle radia depending on how many circles can fit the screen width
+const distanceBetween = 50;
+const circleAmountPerScreen = screenWidth / distanceBetween - 2; // minus 2 circles so there's a little space left
+const radiaArray = Array.from(
+  { length: circleAmountPerScreen <= 31 ? circleAmountPerScreen : 31 },
+  () => distanceBetween
 );
 
-const circle2 = Bodies.circle(
-  screenWidth / 2,
-  screenHeight / 2,
-  10,
-  cicleOptions
-);
+const circlesArray = [];
 
-const testConstraint = Constraint.create({
-  bodyA: circle1,
-  bodyB: circle2,
-  length: 50,
-  stiffness: 1,
-  render: {
-    lineWidth: 0,
-  },
+// Render circles and add to world
+radiaArray.forEach((radius, index) => {
+  let circle;
+  let prevCircle = circlesArray[index - 1];
+  let bitValue = Math.pow(2, index);
+
+  if (index === 0) {
+    circle = Bodies.circle(
+      halfScreenWidth -
+        (distanceBetween / 2) * (radiaArray.length - (index + 1)),
+      halfScreenHeight,
+      radius,
+      {
+        ...cicleOptions,
+        collisionFilter: {
+          category: bitValue,
+        },
+        render: {
+          fillStyle: "#FF0099",
+          type: "line",
+        },
+      }
+    );
+  } else {
+    circle = Bodies.circle(
+      prevCircle.position.x + distanceBetween,
+      halfScreenHeight,
+      radius,
+      {
+        ...cicleOptions,
+        slop: 5,
+        collisionFilter: {
+          category: bitValue,
+          mask:
+            index === 1
+              ? Math.pow(2, index + 2) | Math.pow(2, index - 2)
+              : Math.pow(2, index + 2) | Math.pow(2, index - 2) | headCategory,
+        },
+        render: {
+          fillStyle: "transparent",
+          lineWidth: 2,
+          type: "line",
+        },
+      }
+    );
+  }
+  circlesArray.push(circle);
+  Composite.add(engine.world, circle);
+
+  if (index !== 0) {
+    const circleConstraint = Constraint.create({
+      bodyA: prevCircle,
+      bodyB: circle,
+      length: distanceBetween,
+      stiffness: 1,
+      render: {
+        lineWidth: 0,
+      },
+    });
+    Composite.add(engine.world, circleConstraint);
+  }
 });
-
-Composite.add(engine.world, circle1);
-Composite.add(engine.world, circle2);
-Composite.add(engine.world, testConstraint);
-
-// window.addEventListener("mousedown", (e) => {
-//   Composite.add(engine.world, box(e));
-// });
-
-// window.addEventListener("touchstart", (e) => {
-//   Composite.add(engine.world, box(e));
-// });
 
 var mouse = Mouse.create(render.canvas),
   mouseConstraint = MouseConstraint.create(engine, {
@@ -107,17 +129,11 @@ var mouse = Mouse.create(render.canvas),
     },
   });
 
+// Composite.add(engine.world, ground);
 Composite.add(engine.world, mouseConstraint);
 
-// keep the mouse in sync with rendering
 render.mouse = mouse;
-
-// fit the render viewport to the scene
-// Render.lookAt(render, {
-//   min: { x: 0, y: 0 },
-//   max: { x: 800, y: 600 },
-// });
-
+mouseConstraint.collisionFilter.mask = headCategory;
 // run the renderer
 Render.run(render);
 
@@ -126,18 +142,3 @@ var runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
-
-// var gyroPresent = false;
-// window.addEventListener("devicemotion", function(event){
-//     if(event.rotationRate.alpha || event.rotationRate.beta || event.rotationRate.gamma)
-//         gyroPresent = true;
-// });
-
-// let gyroscope = new Gyroscope({ frequency: 60 });
-
-// gyroscope.addEventListener("reading", (e) => {
-//   console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
-//   console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
-//   console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
-// });
-// gyroscope.start();
