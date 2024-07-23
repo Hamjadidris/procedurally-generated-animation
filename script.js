4e89026c8af64e1d4128d2d13d023dce41e78c4a
Hamjad Idris <dajmahsirdi@gmail.com>
2024-07-23 20:02:16 +0100

    feat: contraints with p5.js

--- script.js
+++ script.js
@@ -1,144 +1,173 @@
 // module aliases
-var Engine = Matter.Engine,
-  Render = Matter.Render,
-  Runner = Matter.Runner,
-  Bodies = Matter.Bodies,
-  Composite = Matter.Composite;
-Constraint = Matter.Constraint;
-MouseConstraint = Matter.MouseConstraint;
-Mouse = Matter.Mouse;
+const {
+  Engine,
+  Runner,
+  World,
+  Bodies,
+  Composite,
+  Render,
+  Constraint,
+  Mouse,
+  MouseConstraint,
+  Common,
+  Vertices,
+} = Matter;
 
-// create an engine
-var engine = Engine.create({
-  gravity: {
-    x: 0,
-    y: 0,
-  },
-});
-
+let engine;
+let canvas;
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
 const halfScreenWidth = window.innerWidth / 2;
 const halfScreenHeight = window.innerHeight / 2;
 
-let canvasOptions = {
+const canvasOptions = {
   height: screenHeight,
   width: screenWidth,
   wireframes: false,
 };
 
-// create a renderer
-var render = Render.create({
-  element: document.body,
-  engine: engine,
-  options: canvasOptions,
-});
-
-let headCategory = 0x0001;
+const headCategory = 0x0001;
 
 const cicleOptions = {
   frictionAir: 0.5,
-  render: {
-    lineWidth: 2,
-    fillStyle: "transparent",
-  },
   restitution: 0.5,
 };
 
 // Make an array of circle radia depending on how many circles can fit the screen width
-const distanceBetween = 50;
+const distanceBetween = 35;
 const circleAmountPerScreen = screenWidth / distanceBetween - 2; // minus 2 circles so there's a little space left
 const radiaArray = Array.from(
   { length: circleAmountPerScreen <= 31 ? circleAmountPerScreen : 31 },
   () => distanceBetween
 );
+// const radiaArray = Array.from(
+//   { length: circleAmountPerScreen <= 31 ? circleAmountPerScreen : 31 },
+//   () => Math.round(Math.random() * distanceBetween)
+// );
 
 const circlesArray = [];
 
 // Render circles and add to world
-radiaArray.forEach((radius, index) => {
-  let circle;
-  let prevCircle = circlesArray[index - 1];
-  let bitValue = Math.pow(2, index);
+function drawCircle() {
+  radiaArray.forEach((radius, index) => {
+    let circle;
+    let prevCircle = circlesArray[index - 1];
+    let bitValue = Math.pow(2, index);
 
-  if (index === 0) {
-    circle = Bodies.circle(
-      halfScreenWidth -
-        (distanceBetween / 2) * (radiaArray.length - (index + 1)),
-      halfScreenHeight,
-      radius,
-      {
-        ...cicleOptions,
-        collisionFilter: {
-          category: bitValue,
-        },
+    if (index === 0) {
+      circle = Bodies.circle(
+        halfScreenWidth -
+          (distanceBetween / 2) * (radiaArray.length - (index + 1)),
+        halfScreenHeight,
+        radius,
+        {
+          ...cicleOptions,
+          collisionFilter: {
+            category: bitValue,
+          },
+        }
+      );
+    } else {
+      circle = Bodies.circle(
+        prevCircle.position.x + distanceBetween,
+        halfScreenHeight,
+        radius,
+        {
+          ...cicleOptions,
+          slop: 1,
+          collisionFilter: {
+            category: bitValue,
+            mask:
+              index === 1
+                ? Math.pow(2, index + 2) | Math.pow(2, index - 2)
+                : Math.pow(2, index + 2) |
+                  Math.pow(2, index - 2) |
+                  headCategory,
+          },
+        }
+      );
+    }
+
+    circlesArray.push(circle);
+    World.add(engine.world, circle);
+
+    if (index !== 0) {
+      const circleConstraint = Constraint.create({
+        bodyA: prevCircle,
+        bodyB: circle,
+        length: distanceBetween,
+        stiffness: 1,
         render: {
-          fillStyle: "#FF0099",
+          anchors: false,
+          lineWidth: 0,
           type: "line",
         },
-      }
-    );
-  } else {
-    circle = Bodies.circle(
-      prevCircle.position.x + distanceBetween,
-      halfScreenHeight,
-      radius,
-      {
-        ...cicleOptions,
-        slop: 5,
-        collisionFilter: {
-          category: bitValue,
-          mask:
-            index === 1
-              ? Math.pow(2, index + 2) | Math.pow(2, index - 2)
-              : Math.pow(2, index + 2) | Math.pow(2, index - 2) | headCategory,
-        },
-        render: {
-          fillStyle: "transparent",
-          lineWidth: 2,
-          type: "line",
-        },
-      }
-    );
-  }
-  circlesArray.push(circle);
-  Composite.add(engine.world, circle);
+      });
+      World.add(engine.world, circleConstraint);
+    }
+  });
+}
 
-  if (index !== 0) {
-    const circleConstraint = Constraint.create({
-      bodyA: prevCircle,
-      bodyB: circle,
-      length: distanceBetween,
-      stiffness: 1,
-      render: {
-        lineWidth: 0,
-      },
-    });
-    Composite.add(engine.world, circleConstraint);
-  }
-});
+function setup() {
+  console.log("first");
+  canvas = createCanvas(screenWidth, screenHeight);
+  // background("#14151f");
 
-var mouse = Mouse.create(render.canvas),
-  mouseConstraint = MouseConstraint.create(engine, {
-    mouse: mouse,
-    constraint: {
-      stiffness: 0.2,
-      render: {
-        visible: false,
-      },
+  engine = Engine.create({
+    gravity: {
+      x: 0,
+      y: 0,
     },
   });
 
-// Composite.add(engine.world, ground);
-Composite.add(engine.world, mouseConstraint);
+  const mouse = Mouse.create(canvas.elt),
+    mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false,
+        },
+      },
+    });
+  World.add(engine.world, mouseConstraint);
+  mouseConstraint.mouse.pixelRatio = pixelDensity();
+  mouseConstraint.collisionFilter.mask = headCategory;
 
-render.mouse = mouse;
-mouseConstraint.collisionFilter.mask = headCategory;
-// run the renderer
-Render.run(render);
+  drawCircle();
+  circlesArray.reverse();
+}
 
-// create runner
-var runner = Runner.create();
+function draw() {
+  background("#14151f");
+  Engine.update(engine);
 
-// run the engine
-Runner.run(runner, engine);
+  stroke(255);
+  strokeWeight(4);
+  fill("#14151f");
+  circlesArray.forEach((circle, i) => {
+    let pos = circle.position;
+    let r = circle.circleRadius;
+    let angle = Math.atan(pos.y / pos.x) * 4;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    ellipse(0, 0, r * 2);
+
+    if (i === circlesArray.length - 1) {
+      stroke("#fff");
+      strokeWeight(10);
+      point(-r / 2, -r + 20);
+      strokeWeight(10);
+      point(-r / 2, r - 20);
+    }
+
+    pop();
+  });
+}
+
+function getParametricCoordinates(radius, angle) {
+  let x = radius * Math.round(Math.cos(angle));
+  let y = radius * Math.sin(angle);
+  return { x, y };
+}
